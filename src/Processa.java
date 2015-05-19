982f7b8c6d537fb0c761cd5eee6e3bec3ea64735
AirtonSilva <airtonsilva268@gmail.com>
2015-05-19 01:07:12 -0300

    Commit Entrada/Saída utilizando Threads

---
+++ src/Processa.java
@@ -0,0 +1,19 @@
+public class Processa {
+	
+	public float soma (float a, float b){
+		return (a+b);
+	}
+	
+	public float diminui (float a, float b){
+		return (a-b);
+	}
+
+	public float divide (float a, float b){
+		return (a/b);
+	}
+	
+	public float multiplica (float a, float b){
+		return (a*b);
+	}
+	
+}
