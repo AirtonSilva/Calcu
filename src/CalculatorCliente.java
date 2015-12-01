bc1b6adafc31bd39d8da6460c045e71a0e80bd66
Airton Silva Braz <airtonsilva268@gmail.com>
2015-11-30 22:58:13 -0300

    Terceiro commit

--- src/CalculatorCliente.java
+++ src/CalculatorCliente.java
@@ -1,3 +1,17 @@
-public class CalculatorCliente {
+import java.*;
+import java.rmi.Naming;
 
+public class CalculatorCliente {
+	
+	public static void main (String[] args){
+		
+	try{
+		
+		Airton_Calculator servidor= (Airton_Calculator) Naming.lookup("rmi://ime.usp.br:2048/ServidorHello");
+		
+	}
+		
+	catch(Exception e){ }
+	
+	}
 }
